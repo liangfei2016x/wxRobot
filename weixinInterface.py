b2080196eeafc2802f170ad8a459f1ca5cadfbbf
liangfei <liangfei2016x@163.com>
2016-07-19 12:56:57 +0800

    weixin1

--- weixinInterface.py
+++ weixinInterface.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
 import hashlib
 import web
 import lxml
@@ -8,33 +8,29 @@ import urllib2,json
 from lxml import etree
 
 class WeixinInterface:
+
     def __init__(self):
         self.app_root = os.path.dirname(__file__)
-        self.templates.root = os.path.join(self.app_root, 'templates')
+        self.templates_root = os.path.join(self.app_root, 'templates')
         self.render = web.template.render(self.templates_root)
 
     def GET(self):
+        #获取输入参数
         data = web.input()
-        signature = data.signature
-        timestamp = data.timestamp
-        nonce = data.nonce
-        echostr = data.echostr
-        token = "testone"
+        signature=data.signature
+        timestamp=data.timestamp
+        nonce=data.nonce
+        echostr=data.echostr
+        #自己的token
+        token="liangfei" #这里改写你在微信公众平台里输入的token
+        #字典序排序
         list=[token,timestamp,nonce]
         list.sort()
-        sha1 = hashlib.sha1()
+        sha1=hashlib.sha1()
         map(sha1.update,list)
-        hashcode = sha1.hexdigest()
+        hashcode=sha1.hexdigest()
+        #sha1加密算法        
 
+        #如果是来自微信的请求，则回复echostr
         if hashcode == signature:
             return echostr
-
-    def POST(self):
-        str_xml = web.data()
-        xml = etree.fromstring(str_xml)
-        content = xml.find("Conetent").text
-        msgType = xml.find("MsgType").text
-        fromUser = xml.find("FromUser").text
-        toUser = xml.find("ToUser").text
-
-        return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中,Hello:"+content)
