f796c63061e84265c0319a1f66765d3917c38697
liangfei <liangfei2016x@163.com>
2016-07-21 13:17:42 +0800

    fanyi

--- weixinInterface.py
+++ weixinInterface.py
@@ -45,6 +45,7 @@ class WeixinInterface:
             content=xml.find("Content").text#获得用户所输入的内容
             content=unicode(content,'utf-8')
             if content[0:2] == u"翻译":
+                post = content[2:]
                 text = post.encode('utf-8')
                 tx = urllib2.quote(text)
                 baseurl=r'http://fanyi.youdao.com/openapi.do?keyfrom=zhilutianshi&key=293831118&type=data&doctype=json&version=1.1&q='
